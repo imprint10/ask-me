35dd4fc14e68863dc7e4188b82940a867eda8e25
Abdallah Abbas <imp.abbass@gmail.com>
2023-05-20 18:51:55 +0300

    update for vercal

--- askme/settings.py
+++ askme/settings.py
@@ -31,7 +31,7 @@ OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 
 # Application definition
